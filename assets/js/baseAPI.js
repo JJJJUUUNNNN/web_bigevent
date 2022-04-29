6860421d8185850c1235487c35deaa67251a0d3a
junjun <1902958347@qq.com>
2022-04-29 21:24:01 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,9 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options){
     options.url='http://www.liulongbin.top:3007'+options.url
+    if(options.url.indexOf('/my/')!==-1){
+        options.headers={
+            Authorization:localStorage.getItem('token')||''
+        }
+    }
 }) 
